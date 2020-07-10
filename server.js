80b83e6030e25b26b50904ee3bc975d2214ddcdb
Nathan Esau <nathan.esau@nielsen.com>
2020-07-10 17:25:26 -0400

    server fix

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 // server.js
 //Install express server
-const express = require(‘express’);
-const http = require(‘http’);
-const path = require(‘path’);
+const express = require('express');
+const http = require('http');
+const path = require('path');
 const app = express();
 // Serve only the static files from the dist directory
-app.use(express.static(__dirname + ‘/dist’));
-app.get(‘*’, (req, res) => {
-  res.sendFile(path.join(__dirname, ‘dist/index.html’));
+app.use(express.static(__dirname + '/dist'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 const port = process.env.PORT || 8080;
 app.set(port);
 const server = http.createServer(app);
-server.listen(port, () => console.log(‘Running…’));
+server.listen(port, () => console.log('Running...'));
